5a245156aa9985fc08b2f408f0cdf508ad9f6fc1
Anup-A-K <anuparunaruna@gmail.com>
2022-11-17 00:19:37 +0530

    task-08

---
+++ task-08/index.js
@@ -0,0 +1,32 @@
+function one()
+{
+    var audio = new Audio("./sounds/crash.mp3");
+    audio.play();
+}
+function two()
+{
+    var audio = new Audio("./sounds/kick-bass.mp3");
+    audio.play();
+}
+function three()
+{
+    var audio = new Audio("./sounds/snare.mp3");
+    audio.play();
+}function four()
+{
+    var audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+}function five()
+{
+    var audio = new Audio("./sounds/tom-2.mp3");
+    audio.play();
+}function six()
+{
+    var audio = new Audio("./sounds/tom-3.mp3");
+    audio.play();
+}
+function seven()
+{
+    var audio = new Audio("./sounds/tom-4.mp3");
+    audio.play();
+}
